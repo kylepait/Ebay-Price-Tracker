a5115b9c25f23087764aba05a160717d83633db9
kylepait <101144609+kylepait@users.noreply.github.com>
2023-12-23 14:02:27 -0500

    Add files via upload

--- ebayPriceTracker-1.py
+++ ebayPriceTracker-1.py
@@ -5,6 +5,8 @@ import numpy as np
 import csv
 from datetime import datetime
 
+from twilio.rest import Client
+
 LINK = "https://www.ebay.com/sch/i.html?_from=R40&_trksid=p4432023.m570.l1312&_nkw=pokemon+black+2+CIB&_sacat=0"
 
 
@@ -38,6 +40,26 @@ def save_to_file(prices):
     with open('prices.csv', 'a', newline='') as f:
         writer = csv.writer(f)
         writer.writerow(fields)
+    
+    send_text_message(f'New entry added:\nTimestamp: {datetime.today()}\nAverage Price: {get_average(prices)}')
+
+
+def send_text_message(message):
+    # Replace these values with your Twilio account SID, auth token, Twilio phone number, and your phone number
+    account_sid = 'ACdcfcbe5482cb9aa111341a21f25bd4df'
+    auth_token = 'ae051fd1e696b90db34b3cfea7c56bef'
+    from_phone_number = '+18555030489'
+    to_phone_number = '+17576513018'
+
+    client = Client(account_sid, auth_token)
+
+    message = client.messages.create(
+        body=message,
+        from_=from_phone_number,
+        to=to_phone_number
+    )
+
+    print(f"Text message sent with SID: {message.sid}")
 
 if __name__ == "__main__":
     prices = get_prices_by_link(LINK)
